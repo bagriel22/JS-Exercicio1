03b62895cda47a6dc678f208b30bc6e79e55bba4
bagriel22 <gabriel.trabalho@outlook.com.br>
2021-01-11 18:31:43 -0300

    first commit

---
+++ exercicio1.js
@@ -0,0 +1,28 @@
+// gabriel está criando um jogo e precisa fazer um sistema que avise o jogador
+//quando suas balas estão acabando 
+// o programa precisa printar na tela RELOAD quando a quantidade de bala for igual ou menor que 5
+// caso contrario o sistema printa na tela FIRE 
+
+
+let quantidadeBalasParaReload = 5
+let balasJogador = 15
+
+if(balasJogador <= quantidadeBalasParaReload ) {
+  console.log("RELOAD")
+}else{
+  console.log ("FIRE")
+ }
+
+
+// ==================================
+
+// GABARITO 
+// let _quantidaDeBalas = 60
+
+// let _quantidadeParaCarregar = 5
+
+// if(_quantidaDeBalas <= _quantidadeParaCarregar) {
+//   console.log("RELOAD")
+// }else{
+//   console.log ("FIRE")
+// }
